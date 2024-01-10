532b4331f57d489db4ce761c1e231587582751f4
Tevfik Emre Sungur <tevfikemresungur@gmail.com>
2024-01-10 17:47:26 +0300

    updated

--- index.py
+++ index.py
@@ -4,70 +4,85 @@ from bs4 import BeautifulSoup
 import warnings
 from urllib3.exceptions import InsecureRequestWarning
 
+book_max = 250
+student_min = 150
+
+city_numbers = [58, 60, 37, 1, 33, 70, 42, 32, 3, 26, 11, 14, 78, 18, 19, 66, 40, 71, 50, 68, 51]
 # Suppress warnings
 warnings.simplefilter('ignore', InsecureRequestWarning)
 
-# Request Data
-url = "https://www.meb.gov.tr/baglantilar/okullar/index.php?ILKODU=50"
-page = requests.get(url)
-soup = BeautifulSoup(page.text, "html")
+print("Started!")
 
-# Extract School Data
-school_html = soup.find_all("tr")
-schools = [[title.text.strip(), title.find_all('a')[1]['href']] for title in school_html[2:]]
-schools = np.array([[*map(lambda x: x.strip(),school[0].split('-')[:3]),school[1]] for school in schools])
+for number in city_numbers:
+    # Request Data
+    url = "https://www.meb.gov.tr/baglantilar/okullar/index.php?ILKODU=" + str(number)
+    page = requests.get(url)
+    soup = BeautifulSoup(page.text, "html")
 
-# Filter Schools
-primary_schools = np.array([school for school in schools if school[2].endswith('İlkokulu')])
-middle_schools = np.array([school for school in schools if school[2].endswith('Ortaokulu')])
+    # Extract School Data
+    school_html = soup.find_all("tr")
+    schools = [[title.text.strip(), title.find_all('a')[1]['href']] for title in school_html[2:]]
+    schools = np.array([[*map(lambda x: x.strip(),school[0].split('-')[:3]),school[1]] for school in schools])
 
-# Refine School Data
-primary_schools_raw = np.array([[school[0], school[1],' '.join(school[2].split(' ')[:-1]), school[3]] for school in primary_schools])
-middle_schools_raw = np.array([[school[0], school[1],' '.join(school[2].split(' ')[:-1]), school[3]] for school in middle_schools])
+    # Filter Schools
+    primary_schools = np.array([school for school in schools if school[2].endswith('İlkokulu')])
+    middle_schools = np.array([school for school in schools if school[2].endswith('Ortaokulu')])
 
-# Find Matching Schools
-matching_schools = np.array([[*item1, item2[3]] for item1 in primary_schools_raw for item2 in middle_schools_raw if np.array_equal(item1[:-1], item2[:-1])])
+    # Refine School Data
+    primary_schools_raw = np.array([[school[0], school[1],' '.join(school[2].split(' ')[:-1]), school[3]] for school in primary_schools])
+    middle_schools_raw = np.array([[school[0], school[1],' '.join(school[2].split(' ')[:-1]), school[3]] for school in middle_schools])
 
-# Scrape Individual School Pages
-book_student_records = []
+    # Find Matching Schools
+    matching_schools = np.array([[*item1, item2[3]] for item1 in primary_schools_raw for item2 in middle_schools_raw if np.array_equal(item1[:-1], item2[:-1])])
 
-for curr_school in matching_schools:
-    curr_book_student_pair_list = []
-    
-    about_url_pre, about_url_mid = curr_school[3:]
+    # Scrape Individual School Pages
+    book_student_records = []
 
-    page_pre = requests.get(about_url_pre, verify=False)
-    page_mid = requests.get(about_url_mid, verify=False)
-
-    soup_pre = BeautifulSoup(page_pre.text, "html.parser")  # should be "html.parser"
-    soup_mid = BeautifulSoup(page_mid.text, "html.parser")  # should be "html.parser"
-
-    try:
-        book_number_pre = int(soup_pre.find("i", class_="fa fa-book").parent.text.split(':')[1].strip())
-    except:
-        book_number_pre = 0
-    
-    try:
-        student_number_pre = int(soup_pre.find("i", class_="fa fa-child").parent.text.split(':')[1].strip())
-    except:
-        student_number_pre = 0
-    
-    print(*curr_school[:3], "İlkokul" ,book_number_pre, student_number_pre)
-    
-    curr_book_student_pair_list.append((book_number_pre,student_number_pre))
-
-    try:
-        book_number_mid = int(soup_mid.find("i", class_="fa fa-book").parent.text.split(':')[1].strip())
-    except:
-        book_number_mid = 0
+    for curr_school in matching_schools:
+        curr_book_student_pair_list = []
         
-    try:
-        student_number_mid = int(soup_mid.find("i", class_="fa fa-child").parent.text.split(':')[1].strip())
-    except:
-        student_number_mid = 0
-    
-    print(*curr_school[:3], "Ortaokul" ,book_number_mid, student_number_mid, "\n")
-    
-    curr_book_student_pair_list.append((book_number_mid,student_number_mid))
+        about_url_pre, about_url_mid = curr_school[3:]
 
-    book_student_records.append(curr_book_student_pair_list)
+        page_pre = requests.get(about_url_pre, verify=False)
+        page_mid = requests.get(about_url_mid, verify=False)
+
+        soup_pre = BeautifulSoup(page_pre.text, "html.parser")  # should be "html.parser"
+        soup_mid = BeautifulSoup(page_mid.text, "html.parser")  # should be "html.parser"
+
+        try:
+            book_number_pre = int(soup_pre.find("i", class_="fa fa-book").parent.text.split(':')[1].strip())
+        except:
+            book_number_pre = 0
+        
+        try:
+            student_number_pre = int(soup_pre.find("i", class_="fa fa-child").parent.text.split(':')[1].strip())
+        except:
+            student_number_pre = 0
+        
+        curr_book_student_pair_list.append((book_number_pre,student_number_pre))
+
+        try:
+            book_number_mid = int(soup_mid.find("i", class_="fa fa-book").parent.text.split(':')[1].strip())
+        except:
+            book_number_mid = 0
+            
+        try:
+            student_number_mid = int(soup_mid.find("i", class_="fa fa-child").parent.text.split(':')[1].strip())
+        except:
+            student_number_mid = 0
+        
+        
+        if book_number_pre == book_number_mid:
+            total_book = book_number_pre
+        else:
+            total_book = book_number_pre + book_number_mid
+            
+        total_student = student_number_pre + student_number_mid
+        
+        if total_book <= book_max and total_student >= student_min:
+            print(*curr_school[:3], "İlkokul" ,book_number_pre, student_number_pre)
+            print(*curr_school[:3], "Ortaokul" ,book_number_mid, student_number_mid, "\n")
+        
+        curr_book_student_pair_list.append((book_number_mid,student_number_mid))
+
+        book_student_records.append(curr_book_student_pair_list)
